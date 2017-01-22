9fbed4c2b06d95a43482be906fb24d532897a6a5
Miguel Company <miguelcompanysoler@gmail.com>
2017-01-22 20:04:58 +0100

    First compilation correct

---
+++ src/main/java/com/company/mywebapp/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.company.mywebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Welcome to My Web App");
+		model.addAttribute("tagline", "The one and only amazing personal web app");
+		return "welcome";
+	}
+}
